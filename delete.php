3687d5b594cbacef3f1213e2baac77cd153c562f
Will C <wchan1@worcester.edu>
2019-12-09 13:58:52 -0500

    edit delete

--- delete.php
+++ delete.php
@@ -1,9 +1,9 @@
 <?php
 require("config.php");
+session_start();
 $id = $_GET['id'];
 $query = "Delete from tasks where id='$id'";
 $statement_delete = $db->prepare($query);
 $result = $statement_delete->execute();
-Header("Location: todolist.php");
 exit();
 ?>
